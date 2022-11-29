dfce7890c3b15d54a95fc0b7c6a76da75df95a89
tunick <dgil1119@ceuandalucia.es>
2022-11-29 20:37:39 +0100

    2037

---
+++ array17/src/array17/array17.java
@@ -0,0 +1,27 @@
+package array17;
+
+import java.util.Scanner;
+
+public class array17 {
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner (System.in);
+		System.out.print("Dame una frase");
+		String frase= scanner.nextLine();
+		
+		String fraseunida= UpperCamelCase(frase);
+		
+		
+			System.out.println(fraseunida);
+		
+		scanner.close();
+	}
+
+	private static String UpperCamelCase(String frase) {
+		// TODO Auto-generated method stub
+		return UpperCamelCase(frase);
+		
+	}
+
+	
+}
+	
